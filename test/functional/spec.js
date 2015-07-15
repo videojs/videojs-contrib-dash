b3694d4b84ce30f7ac5c24a918fdfd40d0c9dacb
jforbes <jforbes@brightcove.com>
2015-07-15 16:01:53 -0400

    Fix non-drm dash tests

--- test/functional/spec.js
+++ test/functional/spec.js
@@ -1,29 +1,33 @@
 var url = require('url'),
   Player = require('videojs-automation'),
-  suiteName, pageUrl;
+  players = [{
+    suiteName: browser.name + ': MPEG-DASH Player',
+    pageUrl: url.resolve(browser.baseUrl, 'test/functional/no-drm-player.html')
+  }];
 
 if (/chrome/i.test(browser.browserName)) {
-  suiteName = browser.name + ': MPEG-DASH Player w/ Widevine DRM';
-  pageUrl = url.resolve(browser.baseUrl, 'test/functional/drm-player.html');
-} else {
-  // Exclude other browsers for now
-  return;
+  players.push({
+    suiteName: browser.name + ': MPEG-DASH Player w/ Widevine DRM',
+    pageUrl: url.resolve(browser.baseUrl, 'test/functional/drm-player.html')
+  });
 }
 
-if (suiteName && pageUrl) {
-  describe(suiteName, function() {
+players.map(function(p) {
+  return describe(p.suiteName, function() {
     var player;
 
     beforeEach(function() {
-      player = new Player(url.resolve(browser.baseUrl, pageUrl));
+      player = new Player(p.pageUrl);
     });
 
-    it('should have no console errors', function() {
-      player.bigPlayButton().click();
-      player.consoleLog().then(function(logs) {
-        expect(logs.length).toBe(0);
+    if (!/explorer/i.test(browser.browserName)) {
+      it('should have no console errors', function() {
+        player.bigPlayButton().click();
+        player.consoleLog().then(function(logs) {
+          expect(logs.length).toBe(0);
+        });
       });
-    });
+    }
 
     it('should have no player errors', function() {
       expect(player.error()).toBeNull();
@@ -34,4 +38,4 @@ if (suiteName && pageUrl) {
       expect(player.isPlaying()).toBe(true);
     });
   });
-}
+});
