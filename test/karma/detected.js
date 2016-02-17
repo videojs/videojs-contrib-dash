ad6d0672f5ba51ef1e74f5b89f23f9f5c56c78e7
Nicky Gerritsen <nicky@streamone.nl>
2016-02-17 08:33:40 +0100

    Use latest firefox and rewrite karma configuration

--- test/karma/detected.js
+++ test/karma/detected.js
@@ -4,29 +4,103 @@ var common = require('./common');
 
 module.exports = function(config) {
 
-  // Travis CI should run in its available Firefox headless browser.
+  var settings = {
+    basePath: '',
+
+    frameworks: ['browserify', 'qunit', 'detectBrowsers'],
+    autoWatch: false,
+    singleRun: true,
+
+    // Compling tests here
+    files: [
+      '../../node_modules/sinon/lib/sinon.js',
+      '../../node_modules/video.js/dist/video.js',
+      '../plugin.test.js',
+      '../integration.test.js',
+      { pattern: '../../src/**/*.js', watched: true, included: false, served: false }
+    ],
+
+    preprocessors: {
+      '../*.js': [ 'browserify' ]
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        require('babelify').configure({
+          sourceMapRelative: './',
+          loose: ['all']
+        })
+      ]
+    },
+
+    plugins: [
+      'karma-qunit',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-ie-launcher',
+      'karma-opera-launcher',
+      'karma-safari-launcher',
+      'karma-browserstack-launcher',
+      'karma-browserify',
+      'karma-detect-browsers'
+    ],
+
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: false
+    },
+
+    reporters: ['dots'],
+
+    browserStack: {
+      name: process.env.TRAVIS_BUILD_NUMBER + process.env.TRAVIS_BRANCH,
+      pollingTimeout: 30000
+    },
+
+    customLaunchers: getCustomLaunchers()
+  };
+
   if (process.env.TRAVIS) {
+    settings.browserify.transform.push('browserify-istanbul');
+    settings.reporters.push('coverage');
 
-    config.set(common({
-      browsers: ['Firefox'],
-      plugins: ['karma-firefox-launcher']
-    }))
-  } else {
-    config.set(common({
-
-      frameworks: ['detectBrowsers'],
-
-      plugins: [
-	'karma-chrome-launcher',
-	'karma-detect-browsers',
-	'karma-firefox-launcher',
-	'karma-ie-launcher',
-	'karma-safari-launcher'
-      ],
-
-      detectBrowsers: {
-	usePhantomJS: false
-      }
-    }));
+    if (process.env.BROWSER_STACK_USERNAME) {
+      settings.detectBrowsers.enabled = false;
+      settings.browsers = [
+        'chrome_bs',
+        'firefox_bs',
+        'safari_bs'
+      ];
+    } else {
+      settings.browsers = ['Firefox'];
+    }
   }
+
+  config.set(settings);
 };
+
+function getCustomLaunchers(){
+  return {
+    chrome_bs: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+
+    firefox_bs: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+
+    safari_bs: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      os: 'OS X',
+      os_version: 'Yosemite'
+    }
+  };
+}
