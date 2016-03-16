5c9fa96f737b3b6ca4831ce902b7a7c206a7f81e
Nathan Weber <nweber@digitalprimates.net>
2016-03-16 08:31:28 -0500

    Check for an ID on the AdaptationSet set the whitelist is set.  If no ID exists, generate one to use for the whitelist dictionary.

--- src/js/whitelist-ext.js
+++ src/js/whitelist-ext.js
@@ -3,6 +3,20 @@
 (function(window, dashjs) {
   'use strict';
 
+  // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+  function generateUUID() {
+    var d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === 'function') {
+      d += window.performance.now(); //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (d + Math.random()*16)%16 | 0;
+      d = Math.floor(d/16);
+      return (c === 'x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  }
+
   var WhitelistPlugin = function () {
     var globalManifest,
         globalDashManifestModel,
@@ -200,7 +214,7 @@
        */
       setWhiteListRepresentations: function (set, representationFilter) {
         whiteList = whiteList || {};
-        var setId = set.id || set,
+        var setId,
             manifest,
             currentAdaptation,
             type,
@@ -208,10 +222,26 @@
             currentRepresentation,
             newIndexInWhiteList,
             representations;
+
+        if (set === undefined || set === null) {
+          throw new Error('AdaptationSet must be defined.');
+        }
+
+        if (typeof set === 'string') {
+          setId = set;
+        }
+        else {
+          // If the Set doesn't have an id, make one and pass it down.
+          if (!set.hasOwnProperty('id')) {
+            set.id = generateUUID();
+          }
+          setId = set.id;
+        }
+
         if (globalManifest) {
           manifest = Object.assign({}, globalManifest.getValue());
           currentAdaptation = globalDashManifestModel.getAdaptationForId(
-                                      parseFloat(setId),
+                                      setId,
                                       manifest,
                                       currentPeriodIndex);
           type = currentAdaptation.mimeType;
@@ -252,7 +282,7 @@
             var manifest = Object.assign({}, globalManifest.getValue()),
                 mediaInfo = globalPlayer.getCurrentTrackFor(type),
                 adaptationSet = globalDashManifestModel.getAdaptationForId(
-                                    parseFloat(mediaInfo.id),
+                                    mediaInfo.id,
                                     manifest,
                                     currentPeriodIndex),
                 proposedRepresentation,
