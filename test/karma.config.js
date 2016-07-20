496f5f38ac5fd78b3fe86deaf5575723e79fa7c9
jforbes <jforbes@brightcove.com>
2016-07-20 17:21:02 -0400

    Just use browsers on travis for build tests

--- test/karma.config.js
+++ test/karma.config.js
@@ -6,15 +6,13 @@ module.exports = function(config) {
       'node_modules/video.js/dist/video-js.css',
       'node_modules/video.js/dist/video.js',
       'node_modules/dashjs/dist/dash.all.debug.js',
-      'dist/videojs-dash.js'].concat(
-        (process.env.BROWSER_STACK_USERNAME || !process.env.TRAVIS) ?
-        ['test/integration.test.js'] : []
-        ).concat([
+      'dist/videojs-dash.js',
+      'test/integration.test.js',
       'test/globals.test.js',
       'test/dashjs.test.js'
-    ]),
+    ],
 
-    frameworks: ['qunit', 'detectBrowsers'],
+    frameworks: ['qunit'],
 
     singleRun: true,
 
@@ -24,36 +22,9 @@ module.exports = function(config) {
 
     browserNoActivityTimeout: 300000,
 
-    detectBrowsers: {
-      enabled: !(process.env.BROWSER_STACK_USERNAME || process.env.TRAVIS),
-      usePhantomJS: false
-    },
-
-    browserStack: {
-      project: 'videojs-contrib-dash',
-      name: process.env.TRAVIS_BUILD_NUMBER + process.env.TRAVIS_BRANCH,
-      pollingTimeout: 30000
-    },
-
-    browsers: process.env.BROWSER_STACK_USERNAME ? ['chrome_bs', 'firefox_bs'] :
-              process.env.TRAVIS ? ['travisChrome'] :
-              ['Chrome'],
+    browsers: process.env.TRAVIS ? ['travisChrome', 'Firefox'] : ['Chrome', 'Firefox'],
 
     customLaunchers: {
-      chrome_bs: {
-        base: 'BrowserStack',
-        browser: 'chrome',
-        os: 'Windows',
-        os_version: '8.1'
-      },
-
-      firefox_bs: {
-        base: 'BrowserStack',
-        browser: 'firefox',
-        os: 'Windows',
-        os_version: '8.1'
-      },
-
       travisChrome: {
         base: 'Chrome',
         flags: ['--no-sandbox']
