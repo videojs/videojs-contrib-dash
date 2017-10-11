be46050882e42bdd0368c3757592b5889ed9bd66
Gary Katsevman <git@gkatsev.com>
2017-10-11 15:27:19 -0400

    revert karma

--- test/karma.config.js
+++ test/karma.config.js
@@ -20,7 +20,9 @@ module.exports = function(config) {
       'dist/videojs-dash.js',
       'test/integration.test.js',
       'test/globals.test.js',
-      'test/dashjs.test.js'
+      'test/dashjs.test.js',
+      'dist-test/browserify.test.js',
+      'dist-test/webpack.test.js'
     ],
     customLaunchers: {
       travisChrome: {
