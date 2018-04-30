f334495ae5f7186d9517eadda77605c2d3b53756
Chuck Wilson <chuck.wilson+github@gmail.com>
2018-04-30 10:05:27 -0400

    bypass autoplay blocking when testing

--- test/karma.config.js
+++ test/karma.config.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
       }
     },
     browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
