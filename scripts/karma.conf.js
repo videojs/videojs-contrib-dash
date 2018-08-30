06d875649e6b0f96a5a768f91a9304c548bb5235
brandonocasey <brandonocasey@gmail.com>
2018-08-30 13:04:00 -0400

    fix package main/module

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -1,16 +1,59 @@
 const generate = require('videojs-generate-karma-config');
 
 module.exports = function(config) {
-  config = generate(config);
-  config.files.unshift('node_modules/dashjs/dist/dash.all.debug.js');
-  config.customLaunchers = {
-    ChromeHeadlessNoSandbox: {
-      base: 'ChromeHeadless',
-      flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
+  const options = {
+    serverBrowsers(defaults) {
+      // ignore defaults and return 'ChromeHeadlessWithFlags'
+      return ['ChromeHeadlessWithFlags'];
+    },
+    files(defaults) {
+      // add in dashjs
+      defaults.unshift('node_modules/dashjs/dist/dash.all.debug.js');
+
+      return defaults;
+    },
+    browsers(browsers) {
+      const chromeIndex = browsers.indexOf('ChromeHeadless');
+      const safariIndex = browsers.indexOf('Safari');
+
+      // change chrome to chrome headless with flags
+      if (chromeIndex !== -1) {
+        browsers.splice(chromeIndex, 1, 'ChromeHeadlessWithFlags');
+      }
+
+      // do not test on safari
+      if (safariIndex !== -1) {
+        browsers.splice(safariIndex, 1);
+      }
+
+      return browsers;
+    },
+    customLaunchers(defaults) {
+      // add no-user-gesture-require variant of chrome
+      return Object.assign(defaults, {
+        ChromeHeadlessWithFlags: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
+        }
+      });
+    },
+    travisLaunchers(defaults) {
+      return {
+        travisChrome: {
+          base: 'ChromeHeadlessWithFlags'
+        },
+        travisFirefox: {
+          base: 'FirefoxHeadless'
+        }
+      };
     }
   };
 
-  config.detectBrowsers.enabled = false;
-  config.browsers = ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'];
-  // any custom stuff here!
+  config = generate(config, options);
+
+  // ignore any console logs except for errors
+  config.browserConsoleLogOptions = {
+    level: 'error',
+    terminal: false
+  };
 };
