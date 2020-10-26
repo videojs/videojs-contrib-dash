0ff1fc3877c9de840bbbe316fcbef2561c3380b9
Alex Barstow <abarstow@brightcove.com>
2020-10-26 12:03:09 -0400

    mute test video so autoplay works

--- test/build.test.js
+++ test/build.test.js
@@ -26,6 +26,7 @@ QUnit.module('Webpack/Browserify Integration', {
     videoEl.setAttribute('controls', '');
     videoEl.setAttribute('width', '600');
     videoEl.setAttribute('height', '300');
+    videoEl.setAttribute('muted', 'true');
     videoEl.className = 'video-js vjs-default-skin';
     this.fixture.appendChild(videoEl);
 
