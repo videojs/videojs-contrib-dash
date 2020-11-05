364e33ec3f2cf6bb408da9304964d35595220029
Garrett Singer <gesinger@gmail.com>
2020-11-05 09:25:35 -0500

    add rollup-plugin-node-builtins and rollup-plugin-node-globals to build

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,11 +1,30 @@
 const generate = require('videojs-generate-rollup-config');
+const nodeBuiltinsPlugin = require('rollup-plugin-node-builtins');
+const nodeGlobalsPlugin = require('rollup-plugin-node-globals');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
 const options = {
   input: 'src/js/videojs-dash.js',
   distName: 'videojs-dash',
-  exportName: 'videojsDash'
+  exportName: 'videojsDash',
+  // stream and string_decoder are used by some modules
+  plugins(defaults) {
+    return {
+      browser: defaults.browser.concat([
+        nodeBuiltinsPlugin(),
+        nodeGlobalsPlugin()
+      ]),
+      module: defaults.module.concat([
+        nodeBuiltinsPlugin(),
+        nodeGlobalsPlugin()
+      ]),
+      test: defaults.test.concat([
+        nodeBuiltinsPlugin(),
+        nodeGlobalsPlugin()
+      ])
+    };
+  }
 };
 const config = generate(options);
 
